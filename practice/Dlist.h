0d23281a0b4996d67f2c20a1796ae444c9904a1a
fws1996 <769718150@qq.com>
2019-01-02 17:37:41 +0800

    添加项目文件。

---
+++ practice/Dlist.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+typedef struct DlistNode
+{
+	struct DlistNode *prev;
+	struct DlistNode *next;
+	int    data;
+}stDlistNode;
+
+typedef struct Dlisthead
+{
+	int size;
+	stDlistNode *head;
+	stDlistNode *tail;
+}stDlistHead;
